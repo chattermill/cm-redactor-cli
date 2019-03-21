16a72f0e0c1561642323976f8f01917c2e854418
Opak Alex <opak.alexandr@gmail.com>
2019-03-21 14:25:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# cm-sensitive-cli
+
+### Install:
+ * `rbenv install 2.6.1`
+
+### Run:
+`ruby cli.rb "My email is contact@chattermill.io"` => `My email is [redacted]`
---
+++ cli.rb
@@ -0,0 +1,24 @@
+require 'yaml'
+
+class Sensitive
+  attr_reader :config
+
+  def initialize
+    @config = YAML::load_file(File.join(__dir__, 'rules.yml'))
+  end
+
+  def rules
+    config['Rules']
+  end
+
+  def remove_sensitive_data str
+    rules.each do |rule|
+      regex = Regexp.new(rule.values.first)
+      str = str&.gsub(regex, '[redacted]')
+    end
+    str
+  end
+
+end
+
+puts Sensitive.new.remove_sensitive_data(ARGV[0])
